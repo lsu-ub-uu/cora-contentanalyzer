93c410dd26badf6327a3d818833dc870191a1dff
perebartrolisimo <pere.bartroli@ub.uu.se>
2024-02-15 16:10:21 +0000

    remove comments

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,18 +6,6 @@ import se.uu.ub.cora.contentanalyzer.ContentAnalyzerInstanceProvider;
  */
 module se.uu.ub.cora.contentanalyzer {
 	requires se.uu.ub.cora.initialize;
-	// requires se.uu.ub.cora.storage;
-	// requires se.uu.ub.cora.initialize;
-	// requires se.uu.ub.cora.fedora;
-	// requires se.uu.ub.cora.converter;
-	// requires se.uu.ub.cora.httphandler;
-	// requires se.uu.ub.cora.xmlconverter;
-	// requires se.uu.ub.cora.logger;
-	//
-	// provides se.uu.ub.cora.storage.archive.RecordArchiveProvider with
-	// FedoraRecordArchiveProvider;
-	// provides se.uu.ub.cora.storage.archive.ResourceArchiveInstanceProvider
-	// with FedoraResourceArchiveProvider;
 
 	uses ContentAnalyzerInstanceProvider;
 
